379ed70d7aa7f3d2eb51f734cbd99c061658e3f6
David Bensabat <david.bensabat@epita.fr>
2018-06-03 07:33:30 +0200

    Searcher done

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Documents-Search-Engine
+
+Resume
+------
+
+This project is about creating a mini search engine, giving a word it has to return the text where the word is existing. The main steps are the connector, the analyzer, the indexer and the searcher. This program has been developed with Python programming language.
+
+The folder contain:
+
+* a src folder containing source file
+* a resources folder containing
+     * texts files
+     * json files (created by the program)
+
+___
+Execution
+---------
+
+Go into root folder and launch the this command:
+
+    > python3 src/main.py
+
+___
+Authors
+-------
+
+EPITA School, SCIA Master 1 - Project for Text Mining Course. 
+
+Authors: **BENSABAT David** (bensab_d)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import os
 import unicodedata
 import re
+import json
 from abc import ABC, abstractmethod
 from pathlib import Path
+
+class bcolors:
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    ENDC = '\033[0m'
 
 class Document:
     def __init__(self, text, url):
@@ -97,33 +103,125 @@ def analyze(documents, processors):
     tokenized_document = TokenizedDocument(words, documents.url)
     return tokenized_document
 
+# Function that build and return an index object
+def build(document_list, posting_list):
+    urlToDid = {}
+    Did = 1
+    for document in document_list:
+        urlToDid[document.url] = Did
+        Did += 1
+
+    wordToDids = {}
+    for posting in posting_list:
+        urls = []
+        for url in posting.urls:
+            urls.append(urlToDid[url])
+
+        # Reduce urls
+        urls = list(set(urls))
+        wordToDids[posting.word] = urls
+
+    index = Index(urlToDid, wordToDids)
+    return index
+
+# Function that save index on json file
+def save(index, path):
+    json_filename_urlToDid = path + "urlToDid.json"
+    json_filename_wordToDids = path + "wordToDids.json"
+
+    with open(json_filename_urlToDid, 'w') as f:
+        json.dump(index.urlToDid, f)
+
+    with open(json_filename_wordToDids, 'w') as f:
+        json.dump(index.wordToDids, f)
+
+# Function that create index from json file
+def load(path):
+    json_filename_urlToDid = path + "urlToDid.json"
+    json_filename_wordToDids = path + "wordToDids.json"
+    
+    with open(json_filename_urlToDid, 'r') as f:
+        datastore_urlToDid = json.load(f)
+
+    with open(json_filename_wordToDids, 'r') as f:
+        datastore_wordToDids = json.load(f)
+
+    index = Index(datastore_urlToDid, datastore_wordToDids)
+    return index
+
+# Function that search urls associated to a word
+def search(word, index_l):
+    try:
+        ids = index_l.wordToDids[word]
+    except:
+        print("This word doesn't exist on our word base, please try another.")
+        return []
+
+    urlToDid_dict = index_l.urlToDid
+    urls = []
+    for id in ids:
+        url = list(urlToDid_dict.keys())[list(urlToDid_dict.values()).index(id)]
+        urls.append(url)
+
+    return urls
+
 def main():
         
-    print("Hello Moteur!")
+    print("\n--- Documents Search Engine ---\n")
 
     # Creating list of documents
-    print("Creating list of documents")
+    print(bcolors.OKGREEN + "Creating " + bcolors.ENDC + "list of documents")
     path = "./resources/20news-bydate-train/talk.politics.misc/"
     document_list = fetch(path)
-    document_list = document_list[:100]
+    document_list = document_list[:200]
 
     # Creating list of processors objects
-    print("Creating list of processors objects")
+    print(bcolors.OKGREEN + "Creating " + bcolors.ENDC + "list of processors objects")
     processors_list = []
     processor_normalizer = Normalizer()
     processors_list.append(processor_normalizer)
 
     # Creating list of tokenized_document
-    print("Creating list of tokenized_document")
+    print(bcolors.OKGREEN + "Creating " + bcolors.ENDC + "list of tokenized_document")
     tokenized_document_list = []
     for document in document_list:
         tokenized_document = analyze(document, processors_list)
         tokenized_document_list.append(tokenized_document)
 
     # Creating posting list
-    print("Creating posting list")
+    print(bcolors.OKGREEN + "Creating " + bcolors.ENDC + "posting list")
     posting_list = index(tokenized_document_list)
-    print("The word {} is in urls {}".format(posting_list[100].word, posting_list[100].urls))
+    #print("The word {} is in urls {}".format(posting_list[100].word, posting_list[100].urls))
+
+    # Creating index
+    print(bcolors.OKGREEN + "Creating " + bcolors.ENDC + "index")
+    index_o = build(document_list, posting_list)
+
+    # Saving index on disk
+    print(bcolors.OKGREEN + "Saving " + bcolors.ENDC + "index on disk")
+    path_index = "./resources/"
+    save(index_o, path_index)
+
+    # Loading index saved from disk
+    print(bcolors.OKGREEN + "Loading " + bcolors.ENDC + "index saved from disk")
+    index_l = load(path_index)
+
+    # Searching user input words
+    word = input("\nPlease enter a word (-quit to exit): ")
+    while word not in ["-Quit", "-quit", "-exit", "-stop"]:
+        print ("You entered the word " + bcolors.OKBLUE + str(word) + bcolors.ENDC + ".")
+
+        for processor in processors_list:
+            word = processor.process(word)
+
+        urls = search(word, index_l)
+        if urls:
+            print("This word appears on these texts: ")            
+            print(urls)
+
+        word = input("\nPlease enter a word (-quit to exit): ")
+    
+    print("\nThanks for use!\n")
 
 
 if __name__== "__main__":
